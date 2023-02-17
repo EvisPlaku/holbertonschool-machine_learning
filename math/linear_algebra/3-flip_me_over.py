a1a4849960cfa6acc609244d431305bb01d0fdd5
evisp <evisplaku@gmail.com>
2023-02-17 08:42:14 -0800

    test

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 """ defines function that transposes a 2D matrix """
 
+#def matrix_transpose(matrix):
+#    """returns the transposed matrix"""
+#    return [[row[idx] for row in matrix] for idx in range(len(matrix[0]))]
+
 def matrix_transpose(matrix):
-    """returns the transposed matrix"""
-    return [[row[idx] for row in matrix] for idx in range(len(matrix[0]))]
+    rows, cols = len(matrix), len(matrix[0])
+    transpose = [[0 for j in range(rows)] for i in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            transpose[j][i] = matrix[i][j]
+    return transpose
