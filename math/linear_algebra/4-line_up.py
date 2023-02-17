664a862f1c781d80ed591d9b17e96a472a17b704
evisp <evisplaku@gmail.com>
2023-02-17 08:45:19 -0800

    add 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 """Add two arrays elementwise"""
 
-
+"""
 def add_arrays(arr1, arr2):
-    """Add two arrays elementwise"""
     if len(arr1) != len(arr2):
         return None
     return [a + b for a, b in zip(arr1, arr2)]
+"""
+
+def add_arrays(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return None
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+    return result
