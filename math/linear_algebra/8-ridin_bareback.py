17f7f3db859c0b4d1468d716c536fecc7b0d7436
evisp <evisplaku@gmail.com>
2023-02-22 00:09:58 -0800

    add matrix mult

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -3,9 +3,15 @@
 
 
 def mat_mul(mat1, mat2):
-    """Multiply two matrices"""
+    """Performs matrix multiplication between two matrices mat1 and mat2."""
+    # Check if the two matrices can be multiplied
     if len(mat1[0]) != len(mat2):
         return None
-    mat2_trans = [[row[idx] for row in mat2] for idx in range(len(mat2[0]))]
-    return [[sum([a * b for a, b in zip(row1, row2)]) for row2 in mat2_trans]
-            for row1 in mat1]
+    # Create an empty result matrix with the appropriate dimensions
+    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+    # Perform the multiplication and store the result in the new matrix
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
