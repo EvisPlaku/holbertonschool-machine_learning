1e3a38161ea58108e5b743bdeb5b67fa26340f45
evisp <evisplaku@gmail.com>
2023-02-27 10:52:39 -0800

    test

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 """ defines a function that calculates the integral of a polynomial """
 
+
 def poly_integral(poly, C=0):
-    # Check if poly is valid
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly):
+    if type(poly) != list or len(poly) == 0:
         return None
 
-    # Check if C is valid
-    if not isinstance(C, int):
-        return None
+    integral = [C]
 
-    # Integrate the polynomial and append the integration constant
-    integral_coeffs = [C]
     for i in range(len(poly)):
-        coeff = poly[i] / (i+1)
-        if coeff.is_integer():
-            coeff = int(coeff)
-        integral_coeffs.append(coeff)
+        coef = poly[i] / (i + 1)
 
-    # Remove trailing zeros
-    while len(integral_coeffs) > 1 and integral_coeffs[-1] == 0:
-        integral_coeffs.pop()
+        if coef.is_integer():
+            coef = int(coef)
 
-    return integral_coeffs
+        integral.append(coef)
+
+    while integral[-1] == 0 and len(integral) > 1:
+        integral.pop()
+
+    return integral
