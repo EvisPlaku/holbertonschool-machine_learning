b307561ba7ce7a9d831b53416caa415990a0eb6a
evisp <evisplaku@gmail.com>
2023-03-15 09:21:49 -0700

    test

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -4,20 +4,21 @@ Defines a function that performs valid convolution
 on a grayscale image
 """
 
+
 import numpy as np
+
 
 def convolve_grayscale_valid(images, kernel):
     """
     Performs a valid convolution on grayscale images
     """
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-    # Compute output size
-    oh, ow = h - kh + 1, w - kw + 1
-    output = np.zeros((m, oh, ow))
-    # Perform convolution for each image
-    for i in range(m):
-        for j in range(oh):
-            for k in range(ow):
-                output[i, j, k] = np.sum(images[i, j:j+kh, k:k+kw] * kernel)
-    return output
+    m, height, width =  images.shape
+    kernel_height, kernel_width  = kernel.shape
+    out_height = height - kernel_height + 1
+    out_width  = width - kernel_width + 1
+    convoluted = np.zeros((m, out_height, out_width))
+    for h in range(out_height):
+        for w in range(out_width):
+            output = np.sum(images[:, height: height + kernel_height, width: width + kernel_width] * kernel, axis=1).sum(axis=1)
+            convoluted[:, height, width] = output
+    return convoluted
