c9f4281287a2a0f794ea3437851ef89eff7375a3
evisp <evisplaku@gmail.com>
2023-03-26 06:06:44 -0700

    add task 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -10,22 +10,20 @@ import numpy as np
 
 def likelihood(x, n, P):
     """Check input types and values"""
-        if not isinstance(n, int) or n <= 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
+    if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not np.all(np.logical_and(P >= 0, P <= 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
-
-    # Calculate the likelihood for each probability in P
-    likelihoods = np.zeros_like(P)
-    for i, p in enumerate(P):
-        numerator = np.prod(np.arange(n-x+1, n+1))
-        denominator = np.prod(np.arange(1, x+1))
-        likelihoods[i] = numerator / denominator * (p ** x) * ((1 - p) ** (n - x))
-
-    return likelihoods
+    for value in P:
+        if value > 1 or value < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+    # likelihood calculated as binomial distribution
+    factorial = np.math.factorial
+    fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
+    likelihood = fact_coefficient * (P ** x) * ((1 - P) ** (n - x))
+    return likelihood
